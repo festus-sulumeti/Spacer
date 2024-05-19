ee8c5de738a513ca9f62b79a436feb03f363193d
festus-sulumeti <sulumetifestus@outlook.com>
2024-05-19 11:38:09 +0300

    modified appjs with the login route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </Router>
   );
