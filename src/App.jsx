eca79eb6c951d0e1629b660989c1040e2083638f
festus-sulumeti <sulumetifestus@outlook.com>
2024-05-19 10:27:03 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+// src/components/Home.jsx
+import { Link } from "react-router-dom";
+import '../styling/Home.css';
+
+const Home = () => {
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <h1 className="heading">SPACER</h1>
+        <br />
+        <p className="description">Discover and book unique spaces for any activity</p>
+        <br />
+        <Link to="/login" className="link login-link">Login</Link>
+        <br />
+        <br />
+        <a href="#" className="link signup-link">Create account</a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
