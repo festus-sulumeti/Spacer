8977547e8a30f84fb9f6ba76ecb25a6a669b8d75
festus-sulumeti <sulumetifestus@outlook.com>
2024-05-19 11:40:48 +0300

    the login

---
+++ src/components/Login.jsx
@@ -0,0 +1,22 @@
+// src/components/Home.jsx
+import { Link } from "react-router-dom";
+import '../styling/Home.css';
+
+const Login = () => {
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <h1 className="heading"> Welcome to Our Platform! </h1>
+        <br />
+        <p className="description">Unlock exclusive features by signing in.</p>
+        <br />
+        <Link to="/login" className="link login-link">User Login</Link>
+        <br />
+        <br />
+        <a href="#" className="link signup-link">Admin Login</a>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
